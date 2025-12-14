a6a54bde44a637ea8cf1eacc05422ca0b63f2b8b
mrdinde <wangjie190518@gmail.com>
2025-12-14 17:17:39 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/german-verbs-pro/', // Replace 'your-repo-name' with your actual GitHub repository name
       server: {
         port: 3000,
         host: '0.0.0.0',
